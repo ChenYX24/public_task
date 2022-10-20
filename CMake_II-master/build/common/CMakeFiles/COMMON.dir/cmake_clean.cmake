c2966698e677db1b0daca8c2189caa16f6c93512
ChenYX24 <90974567+ChenYX24@users.noreply.github.com>
2022-10-20 18:42:07 +0800

    cmake
    有点bug

---
+++ CMake_II-master/build/common/CMakeFiles/COMMON.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/COMMON.dir/rmath/src/rmath.cpp.o"
+  "libCOMMON.pdb"
+  "libCOMMON.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/COMMON.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
